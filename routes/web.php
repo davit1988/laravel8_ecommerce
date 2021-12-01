4843fb7cf5fde89ad4e8569192fe13e79a01dd7b
davit marutyan <davit.marutyan2@gmail.com>
2021-12-01 23:44:42 +0400

    #part3 laravel install bootstrap and multi auth

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,22 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+|https://www.youtube.com/watch?v=Fz0pTzHptGE&list=PL_99hMDlL4d3-n63bsNaaDRnTZdCOvU6q&index=1
+https://www.fundaofwebit.com/laravel-5-8/how-to-make-user-and-admin-login-system-in-laravel
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth','isAdmin']], function () {
+
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    });
+
 });
